029d40b7e1fea515f85aa71b569dcc91b5abcd86
Namjoo Fard Kourosh s338151 <kouroshnamjoofard@gmail.com>
2025-10-15 00:23:28 +0330

    Fixed the localhost address

--- client-totem/src/api/ticketApi.js
+++ client-totem/src/api/ticketApi.js
@@ -1,6 +1,6 @@
 // src/api/ticketApi.js
 
-const API_BASE_URL = "http://localhost:3001/api"; 
+const API_BASE_URL = "http://localhost:3001"; 
 
 export async function getTicket(serviceType) {
   try {
